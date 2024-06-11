1b49f7bc6c8696978f6ace71243e2719fa8699c9
Maryam Ali Asghar <aliasgharmariyam@gmail.com>
2024-06-11 11:53:40 +0500

    comment

---
+++ main.js
@@ -0,0 +1,5 @@
+for (var i = 1; i <= 10; i++) {
+    var number = 2;
+    var Answer = 2 * 1;
+    console.log(number, "", "X", i, " ", "=", Answer);
+}
---
+++ main.ts
@@ -0,0 +1,7 @@
+
+for (let i = 1; i <= 10; i++) {
+    let number = 2;
+    let Answer = 2*1; 
+
+    console.log(number,"", "X" , i , " " ,"=", Answer);
+}
